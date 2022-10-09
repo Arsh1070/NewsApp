368787285b8542fdd85e48e6e88b699645f49baa
Arsh Brar <arshbrarasb626@gmail.com>
2022-10-09 10:03:12 +0530

    minor changes

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -23,7 +23,7 @@ const Login = ({ history }) => {
       setLoading(true);
       const data = await axios
         .post(
-          `/api/v1/login`,
+          `http://localhost:8003/api/v1/login`,
           {
             email,
             password,
