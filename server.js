b5c583f9a15545318df046e7c08f8a8512d5f242
Arsh Brar <arshbrarasb626@gmail.com>
2022-10-09 11:50:53 +0530

    minor changes

--- server.js
+++ server.js
@@ -19,13 +19,19 @@ app.use(errorHandler);
 
 // Serve static assets if in production mode.
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+/* if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-}
+} */
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 8003;
 
