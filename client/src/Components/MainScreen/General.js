c4a7006e48b21c53bc45a184e309d42bd89bc3e9
Arsh Brar <arshbrarasb626@gmail.com>
2022-11-14 12:50:05 +0530

    minor changes

--- client/src/Components/MainScreen/General.js
+++ client/src/Components/MainScreen/General.js
@@ -21,38 +21,50 @@ const General = ({ myCategory }) => {
 
   return (
     <>
-      {newsArray?.map((item, index) => {
-        return (
-          <div className="contentGen" key={`nesw_${index + 1}`}>
-            <img
-              className="imgSGen"
-              src={
-                item?.urlToImage ||
-                "https://as1.ftcdn.net/v2/jpg/02/35/35/40/1000_F_235354051_yz3envzxnH9dulycguP6l4Bh3Xx0BPZ0.jpg"
-              }
-              alt="pic"
-            ></img>
-            <div className="sub-headings">
-              <h3 key={item.id}>{item.title}</h3>
-              <a href={item?.url} className="source">
-                {item?.source?.name}{" "}
-              </a>
-              <span>| {new Date(item?.publishedAt).toLocaleDateString()}</span>
-              <p className="description">
-                {item.description}{" "}
+      {newsArray.length > 0 ? (
+        newsArray?.map((item, index) => {
+          return (
+            <div className="contentGen" key={`nesw_${index + 1}`}>
+              <img
+                className="imgSGen"
+                src={
+                  item?.urlToImage ||
+                  "https://as1.ftcdn.net/v2/jpg/02/35/35/40/1000_F_235354051_yz3envzxnH9dulycguP6l4Bh3Xx0BPZ0.jpg"
+                }
+                alt="pic"
+              ></img>
+              <div className="sub-headings">
+                <h3 key={item.id}>{item.title}</h3>
+                <a href={item?.url} className="source">
+                  {item?.source?.name}{" "}
+                </a>
                 <span>
-                  <a href={item.url} target="_blank" rel="noreferrer">
-                    Read full article...
-                  </a>
+                  | {new Date(item?.publishedAt).toLocaleDateString()}
                 </span>
-              </p>
-              {/*     <a href={item.url} target="_blank" rel="noreferrer">
+                <p className="description">
+                  {item.description}{" "}
+                  <span>
+                    <a href={item.url} target="_blank" rel="noreferrer">
+                      Read full article...
+                    </a>
+                  </span>
+                </p>
+                {/*     <a href={item.url} target="_blank" rel="noreferrer">
                 Continue to read...s
               </a> */}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <>
+          <img
+            className="imgSGen"
+            src="https://img.freepik.com/premium-vector/no-data-empty-data-concept-illustration-vector_610956-1400.jpg?w=1480"
+            alt="pic"
+          />
+        </>
+      )}
     </>
   );
 };
